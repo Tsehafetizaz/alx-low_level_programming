319e227565a56f4087e6fbe59b6be5136a4ea68a
Tsehafetizaz <wendetefe@gmail.com>
2023-07-13 06:18:32 -0700

    65

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,2 @@
 #include <stdio.h>
-int main(void) { return puts("Programming is like building a multilingual puzzle,") != EOF ? 0 : 1; }
-
+int main(void) { return puts("Programming is like building a multilingual puzzle") != EOF ? 0 : 1; }
