f1b9a8b070563f0c5ecbd9d991495bc92b4548bd
Tsehafetizaz <wendetefe@gmail.com>
2023-07-13 06:04:27 -0700

    789

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,2 @@
+#!/bin/bash
+int main(void) { if (printf("Size of a char: %zu byte(s)\nSize of an int: %zu byte(s)\nSize of a long int: %zu byte(s)\nSize of a long long int: %zu byte(s)\nSize of a float: %zu byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float))) {} return 0; }
