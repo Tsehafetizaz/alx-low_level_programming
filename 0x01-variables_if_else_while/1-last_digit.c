540cba61313221405fe9015c76acf52d0fbdbc22
Tsehafetizaz <wendetefe@gmail.com>
2023-07-14 14:01:57 -0700

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,9 +5,8 @@
 /**
  * main - Entry point of the program
  *
- * This program generates a random number, stores it in the variable 'n',
- * and then prints the last digit of 'n' along with additional information
- * based on the value of the last digit.
+ * Tr to the variable n and prints the last digit of n
+ * ath additional information based on the value of the last digit.
  *
  * Return: Always 0 (Success)
  */
@@ -15,17 +14,17 @@ int main(void)
 {
 int n;
 
-srand(time(0)); // Seed the random number generator with current time
-n = rand() - RAND_MAX / 2; // Generate a random number
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-printf("Last digit of %d is %d", n, n % 10); // Print the last digit of n
-
+printf("Last digit of %d is %d", n, n % 10);
+	
 if (n % 10 > 5)
-printf(" and is greater than 5\n"); // Additional information for last digit > 5
+printf(" and is greater than 5\n");
 else if (n % 10 == 0)
-printf(" and is 0\n"); // Additional information for last digit = 0
+printf(" and is 0\n");
 else
-printf(" and is less than 6 and not 0\n"); // Additional information for last digit < 6 and not 0
+printf(" and is less than 6 and not 0\n");
 
 return (0);
 }
