4a3b7bdfb5f0e9ea2645590b494b4c9cb7773057
Tsehafetizaz <wendetefe@gmail.com>
2023-07-18 02:02:08 -0700

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+char *s = "_putchar\n";
+while (*s)
+_putchar(*s++);
+return (0);
+}
