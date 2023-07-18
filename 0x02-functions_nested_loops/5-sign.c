0e3f370b73b7aa163cdc97df127d4516ee6139a3
Tsehafetizaz <wendetefe@gmail.com>
2023-07-18 11:44:37 -0700

    d

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,24 +7,24 @@
 */
 int print_sign(int n)
 {
-for (n < 0);
+if (n < 0)
 
 {
 _putchar ('+ \n');
 return (1);
 
 }
-else if ( n=0 )
+else if ( n = 0 )
 
 {
 _putchar  ('0 \n');
 return (0);
 }
 
-else (n< 0)
+else ( n < 0)
 {
 _putchar ('- \n');
 return (-1);
 }
 }
-
+i
