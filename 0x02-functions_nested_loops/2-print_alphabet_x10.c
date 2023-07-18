5533c58a5f747e451cea1668d973ed8faaa31074
Tsehafetizaz <wendetefe@gmail.com>
2023-07-18 06:44:54 -0700

    3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -24,9 +24,3 @@ for (i = 0; i < 10; i++)
 print_alphabet();
 }
 }
-
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,8 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-
-#endif /* MAIN_H */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
