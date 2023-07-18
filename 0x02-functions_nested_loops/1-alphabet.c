2d8e4d7e804243bc5d696487aee5a0f7f642d8d6
Tsehafetizaz <wendetefe@gmail.com>
2023-07-18 03:58:40 -0700

    8

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
-/*
-*wedew ayisku
+/**
+* main - check the code
+*
+* Return: Always 0.
 */
 
 void print_alphabet(void)
 {
-char letter; 
+char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 _putchar(letter);
