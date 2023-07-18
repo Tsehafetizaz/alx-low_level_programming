615a4c0c6886e2134c812f84872cf0517f032f91
Tsehafetizaz <wendetefe@gmail.com>
2023-07-18 03:37:20 -0700

    55

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+*wedew ayisku
+*/
+
+void print_alphabet(void)
+
+char =letter 
+   
+for (letter='a';letter< 'z'; letter++)
+{
+_putchar(letter);
+_putchar('\n');
+
+return (0);
+}
+
