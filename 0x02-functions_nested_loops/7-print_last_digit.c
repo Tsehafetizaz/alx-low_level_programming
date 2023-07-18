f32bd10ba1ab4a5cf94d9af4d398c3a1f5398b35
Tsehafetizaz <wendetefe@gmail.com>
2023-07-18 12:22:54 -0700

    e

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,7 @@
 * Return: 1 if the number is greater than zero, 0 if the number is zero,
 *         -1 if the number is less than zero.
 */
-int print_last_digit(int n);
+int print_last_digit(int n)
 {
 int last_digit= n %10;
 if (last_digit < 0)
