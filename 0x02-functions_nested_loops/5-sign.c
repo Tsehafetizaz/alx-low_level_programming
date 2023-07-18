350507ac0c2c5a6b6df0ca2b0197736f574ec754
Tsehafetizaz <wendetefe@gmail.com>
2023-07-18 11:41:45 -0700

    d

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* print_sign - prints the sign of a number
+*
+* Return: 1 if the character is a letter, lowercase or uppercase, 0 otherwise.
+*/
+int print_sign(int n)
+{
+for (n < 0);
+
+{
+_putchar ('+ \n');
+return (1);
+
+}
+else if ( n=0 )
+
+{
+_putchar  ('0 \n');
+return (0);
+}
+
+else (n< 0)
+{
+_putchar ('- \n');
+return (-1);
+}
+}
+
