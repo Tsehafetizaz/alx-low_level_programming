c7072671a64f3f299b03732b6d4fb86dc0d0f097
Tsehafetizaz <wendetefe@gmail.com>
2023-07-24 05:21:42 -0700

    hella

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,17 +11,20 @@ void puts_half(char *str)
 int len = 0;
 int n;
 
-while (str[len] != '\0') {
+while (str[len] != '\0')
+{
 len++;
 }
 
-if (len % 2 == 0) {
+if (len % 2 == 0)
+{
 n = len / 2;
 } else {
 n = (len - 1) / 2 + 1;
 }
 
-while (str[n] != '\0') {
+while (str[n] != '\0')
+{
 _putchar(str[n]);
 n++;
 }
