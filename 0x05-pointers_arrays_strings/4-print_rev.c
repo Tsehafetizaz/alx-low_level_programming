c0dc1dd8a9c801be3bb8768b9e46db618d03662a
Tsehafetizaz <wendetefe@gmail.com>
2023-07-24 04:50:06 -0700

    d

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,18 +10,18 @@ void print_rev(char *s)
 {
 int length = 0;
 
-// find the length of the string
+
 while (s[length] != '\0')
 {
 length++;
 }
 
-// print the string in reverse
+
 for (int i = length - 1; i >= 0; i--)
 {
 _putchar(s[i]);
 }
 
-// print a newline
+
 _putchar('\n');
 }
