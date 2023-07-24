76a7790f8414ce2208d258f1c200a4dac8a9f655
Tsehafetizaz <wendetefe@gmail.com>
2023-07-24 04:48:20 -0700

    fitsum

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: the string to print in reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int length = 0;
+
+// find the length of the string
+while (s[length] != '\0')
+{
+length++;
+}
+
+// print the string in reverse
+for (int i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+
+// print a newline
+_putchar('\n');
+}
