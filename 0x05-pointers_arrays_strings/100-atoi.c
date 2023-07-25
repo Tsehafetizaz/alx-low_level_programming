959e45b9b9853cf8f0ed968580ead41bc1a7b83d
Tsehafetizaz <wendetefe@gmail.com>
2023-07-24 23:41:40 -0700

    dad

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,20 +1,26 @@
 #include <limits.h>
 
-int _atoi(char *s) {
+int _atoi(char *s)
+{
 int result = 0;
 int sign = 1;
 int i = 0;
-while (s[i] != '\0' && (s[i] < '0' || s[i] > '9')) {
-if (s[i] == '-') {
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+{
+if (s[i] == '-')
+{
 sign = sign * -1;
 }
-else if (s[i] == '+') {
+else if (s[i] == '+')
+{
 sign = sign * 1;
 }
 i++;
 }
-while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9') {
-if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)) {
+while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+{
+if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+{
 return (sign == 1) ? INT_MAX : INT_MIN;
 }
 result = result * 10 + (s[i++] - '0');
