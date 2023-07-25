89b30963f2f66c3d8b4b685cb98105e27e9268c8
Tsehafetizaz <wendetefe@gmail.com>
2023-07-24 23:44:28 -0700

    dad

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,11 +19,12 @@ i++;
 }
 while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
 {
-if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+if (result > INT_MAX / 10 ||
+(result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
 {
-return (sign == 1) ? INT_MAX : INT_MIN;
+return ((sign == 1) ? INT_MAX : INT_MIN);
 }
 result = result * 10 + (s[i++] - '0');
 }
-return result * sign;
+return (result *sign);
 }
