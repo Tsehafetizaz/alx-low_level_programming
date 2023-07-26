b94194f5bee4ee1f311b661cc0eaa648ff5480ae
Tsehafetizaz <wendetefe@gmail.com>
2023-07-26 04:25:20 -0700

    d

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+# include "main.h"
+
+/* 
+**_strcat concatenates two strings.
+*dest *src are two strings
+*return value
+*/
+
+
+
+char *_strcat(char *dest, char *src);
+{
+
+char *dest[20] = "Hello";
+char *src[20] = "World";
+*_strcat(*dest, *src);
+printf("%s\n", *dest);
+
+return (0);
+
+}
