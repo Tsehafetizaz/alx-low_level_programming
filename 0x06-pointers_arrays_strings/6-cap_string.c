08c2ec7cf6072938234c35d03b352896f47c2f4e
Tsehafetizaz <wendetefe@gmail.com>
2023-07-27 06:32:55 -0700

    ths

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
 * cap_string - capitalizes all words of a string.
@@ -7,20 +6,39 @@
 *
 * Return: the pointer to the string
 */
+int is_alpha(char c) {
+return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+int to_upper(char c) {
+if (c >= 'a' && c <= 'z')
+return c - 'a' + 'A';
+return c;
+}
+
+int is_separator(char c) {
+char separators[] = " \t\n,;.!?\"(){}";
+for (int i = 0; separators[i]; i++) {
+if (separators[i] == c) return 1;
+}
+return 0;
+}
+
+
 char *cap_string(char *str)
 {
 int i;
 int new_word = 1;
-char separators[] = " \t\n,;.!?\"(){}";
+
 for (i = 0; str[i] != '\0'; i++)
 {
-if (new_word && isalpha((int)str[i]))
+if (new_word && is_alpha(str[i]))
 {
-str[i] = toupper((int)str[i]);
+str[i] = to_upper(str[i]);
 new_word = 0;
 }
 
-if (strchr(separators, str[i]))
+if (is_separator(str[i]))
 {
 new_word = 1;
 }
