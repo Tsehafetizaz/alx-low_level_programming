e7a8217a67eca9540c5bb25414b3ac06a3b3bf68
Tsehafetizaz <wendetefe@gmail.com>
2023-07-27 06:50:49 -0700

    dg

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,6 @@
 /**
 * cap_string - capitalizes all words in a string.
 * @str: the string to capitalize.
-*
 * Return: pointer to the capitalized string.
 */
 char *cap_string(char *str)
@@ -13,16 +12,20 @@ int start_word = 1;
 
 while (str[i] != '\0')
 {
-if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+str[i] == '}')
 {
 start_word = 1;
 }
-else if (start_word == 1)
-{
-if (str[i] >= 'a' && str[i] <= 'z')
+else if (start_word == 1 && str[i] >= 'a' && str[i] <= 'z')
 {
 str[i] = str[i] - 'a' + 'A';
+start_word = 0;
 }
+else
+{
 start_word = 0;
 }
 i++;
