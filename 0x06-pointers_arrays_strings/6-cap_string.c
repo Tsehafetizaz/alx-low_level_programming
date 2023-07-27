420a1e8041d9aa3250a543521e672c961d6a8fcd
Tsehafetizaz <wendetefe@gmail.com>
2023-07-27 06:26:55 -0700

    dsaga

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,30 @@
 #include "main.h"
+#include <ctype.h>
+
 /**
-* cap_string - Capitalizes all words of a string.
-* @s: The string to change.
+* cap_string - capitalizes all words of a string.
+* @str: the string to capitalize
 *
-* This function converts the first letter of each word
-* in the string to uppercase if it is a lowercase letter.
-*
-* A "word" is defined as a sequence of characters separated
-* by one of the following: space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, or }
-*
-* Return: Pointer to the modified string.
+* Return: the pointer to the string
 */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-int i = 0;
+int i;
 int new_word = 1;
-while (s[i])
+char separators[] = " \t\n,;.!?\"(){}";
+for (i = 0; str[i] != '\0'; i++)
 {
-if (s[i] >= 'a' && s[i] <= 'z' && new_word)
+if (new_word && isalpha((int)str[i]))
 {
-s[i] -= ('a' - 'A');
+str[i] = toupper((int)str[i]);
 new_word = 0;
 }
-if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+
+if (strchr(separators, str[i]))
 {
 new_word = 1;
 }
-else
-{
-new_word = 0;
-}
-i++;
-}
-return (s);
 }
 
+return (str);
+}
