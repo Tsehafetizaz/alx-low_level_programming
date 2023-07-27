068bed777ba7d444568233851ee59fe8aa361195
Tsehafetizaz <wendetefe@gmail.com>
2023-07-27 06:18:52 -0700

    daddg

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,20 @@
 #include "main.h"
-
 /**
-* cap_string - a function that capitalizes all words of a string
-* @s: the string to change
+* cap_string - Capitalizes all words of a string.
+* @s: The string to change.
 *
-* Return: pointer to the string.
+* This function converts the first letter of each word
+* in the string to uppercase if it is a lowercase letter.
+*
+* A "word" is defined as a sequence of characters separated
+* by one of the following: space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, or }
+*
+* Return: Pointer to the modified string.
 */
 char *cap_string(char *s)
 {
 int i = 0;
 int new_word = 1;
-
 while (s[i])
 {
 if (s[i] >= 'a' && s[i] <= 'z' && new_word)
@@ -18,9 +22,7 @@ if (s[i] >= 'a' && s[i] <= 'z' && new_word)
 s[i] -= ('a' - 'A');
 new_word = 0;
 }
-if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || 
-s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || 
-s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
 {
 new_word = 1;
 }
@@ -32,3 +34,4 @@ i++;
 }
 return (s);
 }
+
