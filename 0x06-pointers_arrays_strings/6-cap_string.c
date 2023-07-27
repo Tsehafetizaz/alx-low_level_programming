d12e1edb7c27dba5264bc162cdc9809155b0777c
Tsehafetizaz <wendetefe@gmail.com>
2023-07-27 06:47:48 -0700

    adgah

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,48 +1,31 @@
 #include "main.h"
 
 /**
-* cap_string - capitalizes all words of a string.
-* @str: the string to capitalize
+* cap_string - capitalizes all words in a string.
+* @str: the string to capitalize.
 *
-* Return: the pointer to the string
+* Return: pointer to the capitalized string.
 */
-int is_alpha(char c) {
-return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-}
-
-int to_upper(char c) {
-if (c >= 'a' && c <= 'z')
-return c - 'a' + 'A';
-return c;
-}
-
-int is_separator(char c) {
-char separators[] = " \t\n,;.!?\"(){}";
-for (int i = 0; separators[i]; i++) {
-if (separators[i] == c) return 1;
-}
-return 0;
-}
-
-
 char *cap_string(char *str)
 {
-int i;
-int new_word = 1;
+int i = 0;
+int start_word = 1;
 
-for (i = 0; str[i] != '\0'; i++)
+while (str[i] != '\0')
 {
-if (new_word && is_alpha(str[i]))
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 {
-str[i] = to_upper(str[i]);
-new_word = 0;
+start_word = 1;
 }
-
-if (is_separator(str[i]))
+else if (start_word == 1)
 {
-new_word = 1;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 'a' + 'A';
 }
+start_word = 0;
 }
-
+i++;
+}
 return (str);
 }
