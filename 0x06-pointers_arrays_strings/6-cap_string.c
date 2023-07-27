f4c57ae7574c8e4d943d4782245100760d9239cf
Tsehafetizaz <wendetefe@gmail.com>
2023-07-27 06:09:13 -0700

    dagaa

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,7 @@ char *cap_string(char *s)
 {
 int i = 0;
 int new_word = 1;
+
 while (s[i])
 {
 if (s[i] >= 'a' && s[i] <= 'z' && new_word)
@@ -23,11 +24,11 @@ s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
 {
 new_word = 1;
 }
-else {
+else
+{
 new_word = 0;
 }
 i++;
 }
 return (s);
 }
-
