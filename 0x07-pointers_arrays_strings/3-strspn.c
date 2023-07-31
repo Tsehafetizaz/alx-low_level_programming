b0c461da81cd781b1e30aa2e8fe2c150e8d195a0
Tsehafetizaz <wendetefe@gmail.com>
2023-07-31 12:22:40 -0700

    kj

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
-* _strspn - gets the length of a prefix substring
-* @s: string to be scanned
-* @accept: string containing the characters to match
-* Return: number of bytes in the initial segment of s
-* which consist only of bytes from accept
+* _strspn - Gets the length of a prefix substring.
+* @s: The string to be scanned.
+* @accept: The string containing the characters to match.
+* Return: The number of bytes in the initial segment of @s
+* which consist only of bytes from @accept.
 */
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i, j;
-
 for (i = 0; s[i]; i++)
 {
 for (j = 0; accept[j]; j++)
