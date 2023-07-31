f1c03f92561d9a60787cad05ce588749f7922274
Tsehafetizaz <wendetefe@gmail.com>
2023-07-31 06:24:01 -0700

    hhh

---
+++ 0x04-more_functions_nested_loops/wende.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+
+*
+
+*/
+
+void more_numbers(void);
+{
+int i, j;
+for (j=0; j<=10: j++)
+	{
+{
+	for (i=0; i<=14: i++)
+	
+	_put char (i+'0');
+	}
+	_put char ('/n');
+}
+return (0);
+}
