01245e0ed614d71ad82e171681c02000f45fa829
Tsehafetizaz <wendetefe@gmail.com>
2023-07-31 12:48:39 -0700

    ddd

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
 * @s: The string to be scanned
 * @accept: The string containing the characters to match
 *
-* Return: a pointer to the byte in `s` that matches one of the bytes in `accept`
+* Return: a pbytes in `accept`
 * or NULL if no such byte is found
 */
 char *_strpbrk(char *s, char *accept)
