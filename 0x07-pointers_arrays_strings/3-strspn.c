08641f4624373a9655b6e0584df84284177e90cf
Tsehafetizaz <wendetefe@gmail.com>
2023-07-31 12:30:38 -0700

    halla

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,29 @@
 #include "main.h"
 
 /**
-* _strspn - Gets the length of a prefix substring.
-* @s: The string to be scanned.
-* @accept: The string containing the characters to match.
-* Return: The number of bytes in the initial segment of @s
-* which consist only of bytes from @accept.
+* _strspn - gets the length of a prefix substring
+* @s: string to be scanned
+* @accept: string containing the list of characters to match in s
+* 
+* Return: the number of bytes in the initial segment of s 
+* which consist only of bytes from accept
 */
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i, j;
-for (i = 0; s[i]; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; accept[j]; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 {
 break;
 }
 }
-
-if (!accept[j])
+if (accept[j] == '\0')
 {
 return i;
 }
 }
-
 return i;
 }
