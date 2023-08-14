4287cd31e68a47c301b79b21e50dff1155ceff4c
Tsehafetizaz <wendetefe@gmail.com>
2023-08-14 03:53:25 -0700

    sg

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,25 +2,10 @@
 #include <stdlib.h>
 
 /**
-* _strlen - returns the length of a string
-* @s: the string to check
-*
-* Return: the length of the string
-*/
-int _strlen(char *s)
-{
-int len = 0;
-while (*s++)
-len++;
-return (len);
-}
-
-/**
 * new_dog - creates a new dog
 * @name: name of the dog
 * @age: age of the dog
 * @owner: owner of the dog
-*
 * Return: pointer to the new dog
 */
 dog_t *new_dog(char *name, float age, char *owner)
