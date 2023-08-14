283fb9ab228f32c464ede73943c8277f275c73b9
Tsehafetizaz <wendetefe@gmail.com>
2023-08-14 03:36:58 -0700

    dfa

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,58 +1,53 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
 * new_dog - creates a new dog
 * @name: name of the dog
 * @age: age of the dog
 * @owner: owner of the dog
-* Return: pointer to the new dog, or NULL if fails
+* 
+* Return: pointer to the new dog
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *newdog;
-char *name_copy, *owner_copy;
+dog_t *dog;
+char *copy_name, *copy_owner;
 
-newdog = malloc(sizeof(dog_t));
-if (!newdog)
+dog = malloc(sizeof(dog_t));
+if (!dog)
 return (NULL);
 
 if (name)
 {
-for (name_copy = name; *name_copy; name_copy++)
-;
-name_copy = malloc(name_copy - name + 1);
-if (!name_copy)
+copy_name = strdup(name);
+if (!copy_name)
 {
-free(newdog);
+free(dog);
 return (NULL);
 }
-for (name_copy = name; *name_copy; name_copy++, name++)
-*name_copy = *name;
 }
 else
-name_copy = NULL;
+copy_name = NULL;
 
 if (owner)
 {
-for (owner_copy = owner; *owner_copy; owner_copy++)
-;
-owner_copy = malloc(owner_copy - owner + 1);
-if (!owner_copy)
+copy_owner = strdup(owner);
+if (!copy_owner)
 {
-free(name_copy);
-free(newdog);
+free(copy_name);
+free(dog);
 return (NULL);
 }
-for (owner_copy = owner; *owner_copy; owner_copy++, owner++)
-*owner_copy = *owner;
 }
 else
-owner_copy = NULL;
+copy_owner = NULL;
 
-newdog->name = name_copy;
-newdog->age = age;
-newdog->owner = owner_copy;
+dog->name = copy_name;
+dog->age = age;
+dog->owner = copy_owner;
 
-return (newdog);
+return (dog);
 }
+
