372e7db0cc10efa2154683f969ad8202d011119a
Tsehafetizaz <wendetefe@gmail.com>
2023-08-30 00:13:00 -0700

    hope

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,32 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h> /* For size_t */
+#include <stdlib.h>
 
 /**
-* struct list_s - singly linked list
-* @str: string - (malloc'ed string)
-* @len: length of the string
-* @next: points to the next node
-* Description: singly linked list node structure
-*/
-typedef struct list_s
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
-} list_t;
+int n;
+struct listint_s *next;
+} listint_t;
 
-/* Function prototypes */
-
-list_t *create_node(const char *str);
-/* Add other function prototypes as needed */
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -42,4 +30,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif /* LISTS_H */
