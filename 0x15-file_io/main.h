0e8bdf4a9ab4fa8cec274d6c7cdbd9949f5647a5
Tsehafetizaz <wendetefe@gmail.com>
2023-09-04 07:11:14 -0700

    edk

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-
+int main(int argc, char **argv);
+int open_files(char **argv);
+void close_files(int from_fd, int to_fd);
+void copy_content(int from_fd, int to_fd);
 #endif /* MAIN_H */
 
