e333edffe53103a9859b59038435b97715d77c8e
Tsehafetizaz <wendetefe@gmail.com>
2023-09-04 07:09:08 -0700

    ksk

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,89 +1,20 @@
 #include "main.h"
 
-/**
-* open_files - Open source and dest files.
-* @source: Name of the source file.
-* @dest: Name of the destination file.
-* Return: Tuple with the file descriptors of source and destination.
-*/
-int *open_files(char *source, char *dest)
-{
-static int fd[2];
+#define BUFFER_SIZE 1024
 
-fd[0] = open(source, O_RDONLY);
-if (fd[0] == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from %s\n", source);
-exit(98);
-}
-
-fd[1] = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (fd[1] == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
-exit(99);
-}
-
-return (fd);
-}
+int open_files(char **argv);
+void close_files(int from_fd, int to_fd);
+void copy_content(int from_fd, int to_fd);
 
 /**
-* copy_contents - Handles reading and writing between files.
-* @file_from: File descriptor for source file.
-* @file_to: File descriptor for destination file.
-*/
-void copy_contents(int file_from, int file_to)
+ * main - Entry point.
+ * @argc: Number of arguments.
+ * @argv: Argument values.
+ * Return: 0 on success, other values on failure.
+ */
+int main(int argc, char **argv)
 {
-ssize_t read_count, write_count;
-char buffer[1024];
-
-do {
-read_count = read(file_from, buffer, 1024);
-if (read_count > 0)
-{
-write_count = write(file_to, buffer, read_count);
-if (write_count != read_count)
-break;
-}
-} while (read_count > 0);
-
-if (read_count == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read\n");
-exit(98);
-}
-
-if (write_count != read_count)
-{
-dprintf(STDERR_FILENO, "Error: Can't write\n");
-exit(99);
-}
-}
-
-/**
-* close_files - Closes files after operation.
-* @file_from: File descriptor for source file.
-* @file_to: File descriptor for destination file.
-*/
-void close_files(int file_from, int file_to)
-{
-if (close(file_from) == -1 || close(file_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-file_from == -1 ? file_from : file_to);
-exit(100);
-}
-}
-
-/**
-* main - Entry point for file copy program.
-* @argc: Argument count.
-* @argv: Argument vector.
-* Return: 0 on success, error code otherwise.
-*/
-int main(int argc, char *argv[])
-{
-int *fd;
+int from_fd, to_fd;
 
 if (argc != 3)
 {
@@ -91,9 +22,82 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
-fd = open_files(argv[1], argv[2]);
-copy_contents(fd[0], fd[1]);
-close_files(fd[0], fd[1]);
+from_fd = open_files(argv);
+to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+if (to_fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+
+copy_content(from_fd, to_fd);
+close_files(from_fd, to_fd);
 
 return (0);
+}
+
+/**
+ * open_files - Opens the source file.
+ * @argv: Argument values.
+ * Return: File descriptor of the source file.
+ */
+int open_files(char **argv)
+{
+int from_fd = open(argv[1], O_RDONLY);
+
+if (from_fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+return (from_fd);
+}
+
+/**
+ * copy_content - Copies content from source to destination file.
+ * @from_fd: Source file descriptor.
+ * @to_fd: Destination file descriptor.
+ */
+void copy_content(int from_fd, int to_fd)
+{
+int r, w;
+char buffer[BUFFER_SIZE];
+
+while ((r = read(from_fd, buffer, BUFFER_SIZE)) > 0)
+{
+w = write(to_fd, buffer, r);
+if (w != r)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+}
+
+if (r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+}
+
+/**
+ * close_files - Closes the source and destination files.
+ * @from_fd: Source file descriptor.
+ * @to_fd: Destination file descriptor.
+ */
+void close_files(int from_fd, int to_fd)
+{
+if (close(from_fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
+exit(100);
+}
+
+if (close(to_fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
+exit(100);
+}
 }
