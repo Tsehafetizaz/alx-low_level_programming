fefe46cc85aeec8515407df568aeec7d48d694ce
Tsehafetizaz <wendetefe@gmail.com>
2023-09-04 08:05:44 -0700

    cdk

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,75 +1,81 @@
 #include "main.h"
-#define BUF_SIZE 1024
+
+#define BUFFER_SIZE 1024
 
 /**
- * check_argc - Checks for correct argument count.
- * @argc: Argument count.
+ * read_and_write - Reads content from a source file descriptor
+ * and writes it to a destination file descriptor.
+ * @from_fd: File descriptor to read from.
+ * @to_fd: File descriptor to write to.
+ * Return: 0 on success, -1 on failure.
  */
-void check_argc(int argc)
+int read_and_write(int from_fd, int to_fd)
 {
+int r, w;
+char buffer[BUFFER_SIZE];
+while ((r = read(from_fd, buffer, BUFFER_SIZE)) > 0)
+{
+w = write(to_fd, buffer, r);
+if (w != r)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to fd %d\n", to_fd);
+return (-1);
+}
+}
+if (r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from fd %d\n", from_fd);
+return (-1);
+}
+return (0);
+}
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: The number of arguments passed to the program.
+ * @argv: The argument vector.
+ * Return: 0 if successful, or one of the error codes if an error occurs.
+ */
+int main(int argc, char *argv[])
+{
+int from_fd, to_fd;
+
+/* Check for the correct number of arguments */
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
+
+/* Open the source file */
+from_fd = open(argv[1], O_RDONLY);
+if (from_fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
 
-/**
- * error_check - Checks and handles errors.
- * @chk: Error check condition.
- * @file: File related to the error.
- * @fd: File descriptor.
- */
-void error_check(int chk, char *file, int fd)
+/* Open the destination file with appropriate flags and permissions. */
+to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (to_fd == -1)
 {
-if (!chk)
-return;
-if (!file)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
-if (fd == -1)
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file), exit(98);
-else
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file), exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
 }
 
-/**
- * read_and_write - reads from a file and writes to another.
- * @from: file descriptor to read from.
- * @to: file descriptor to write to.
- * @file_from: Source file name.
- * @file_to: Destination file name.
- */
-void read_and_write(int from, int to, char *file_from, char *file_to)
+/* Call helper function */
+if (read_and_write(from_fd, to_fd) == -1)
+exit(98);
+/* Close file descriptors and handle any errors. */
+if (close(from_fd) == -1)
 {
-int r, w;
-char buffer[BUF_SIZE];
-while ((r = read(from, buffer, BUF_SIZE)) > 0)
-{
-w = write(to, buffer, r);
-if (w != r)
-error_check(1, file_to, to);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
+exit(100);
 }
-if (r == -1)
-error_check(1, file_from, from);
-}
-
-/**
- * main - Main function, copies a file.
- * @argc: Argument count.
- * @argv: Argument values.
- * Return: 0 on success.
- */
-int main(int argc, char *argv[])
+if (close(to_fd) == -1)
 {
-int from, to;
-check_argc(argc);
-from = open(argv[1], O_RDONLY);
-error_check(from == -1, argv[1], from);
-to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-error_check(to == -1, argv[2], to);
-read_and_write(from, to, argv[1], argv[2]);
-error_check(close(from) == -1, NULL, from);
-error_check(close(to) == -1, NULL, to);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
+exit(100);
+}
 return (0);
 }
-
