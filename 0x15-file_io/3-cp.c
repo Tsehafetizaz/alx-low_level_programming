c81f1291d434e9969d3205842bf0b22f66bca64a
Tsehafetizaz <wendetefe@gmail.com>
2023-09-04 06:50:07 -0700

    kkl

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,66 +1,99 @@
 #include "main.h"
 
 /**
- * main - Entry point. Program to copy content from one file to another.
- * @argc: Number of arguments.
- * @argv: Array of arguments.
- * Return: 0 on success, or exit with specified error code.
- */
-int main(int argc, char *argv[])
+* open_files - Open source and dest files.
+* @source: Name of the source file.
+* @dest: Name of the destination file.
+* Return: Tuple with the file descriptors of source and destination.
+*/
+int *open_files(char *source, char *dest)
 {
-int file_from, file_to;
+static int fd[2];
+
+fd[0] = open(source, O_RDONLY);
+if (fd[0] == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from %s\n", source);
+exit(98);
+}
+
+fd[1] = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (fd[1] == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+exit(99);
+}
+
+return (fd);
+}
+
+/**
+* copy_contents - Handles reading and writing between files.
+* @file_from: File descriptor for source file.
+* @file_to: File descriptor for destination file.
+*/
+void copy_contents(int file_from, int file_to)
+{
 ssize_t read_count, write_count;
 char buffer[1024];
+
+do {
+read_count = read(file_from, buffer, 1024);
+if (read_count > 0)
+{
+write_count = write(file_to, buffer, read_count);
+if (write_count != read_count)
+break;
+}
+} while (read_count > 0);
+
+if (read_count == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read\n");
+exit(98);
+}
+
+if (write_count != read_count)
+{
+dprintf(STDERR_FILENO, "Error: Can't write\n");
+exit(99);
+}
+}
+
+/**
+* close_files - Closes files after operation.
+* @file_from: File descriptor for source file.
+* @file_to: File descriptor for destination file.
+*/
+void close_files(int file_from, int file_to)
+{
+if (close(file_from) == -1 || close(file_to) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+file_from == -1 ? file_from : file_to);
+exit(100);
+}
+}
+
+/**
+* main - Entry point for file copy program.
+* @argc: Argument count.
+* @argv: Argument vector.
+* Return: 0 on success, error code otherwise.
+*/
+int main(int argc, char *argv[])
+{
+int *fd;
 
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
 
-file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (file_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
-
-while ((read_count = read(file_from, buffer, 1024)) > 0)
-{
-write_count = write(file_to, buffer, read_count);
-if (write_count != read_count)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file_from);
-close(file_to);
-exit(99);
-}
-}
-
-if (read_count == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-
-if (close(file_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
-
-if (close(file_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-exit(100);
-}
+fd = open_files(argv[1], argv[2]);
+copy_contents(fd[0], fd[1]);
+close_files(fd[0], fd[1]);
 
 return (0);
 }
-
