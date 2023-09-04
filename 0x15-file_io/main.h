6d5b9da349d8e8905643673ca63cd85da66d4f12
Tsehafetizaz <wendetefe@gmail.com>
2023-09-04 12:44:45 -0700

    jj

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,7 +18,6 @@ void close_files(int from_fd, int to_fd);
 void copy_content(int from_fd, int to_fd);
 void check_argc(int argc);
 void error_check(int chk, char *file, int fd);
-void read_and_write(int from, int to, char *file_from, char *file_to);
 int main(int argc, char *argv[]);
 int read_and_write(int from_fd, int to_fd);
 void check_argc(int argc);
