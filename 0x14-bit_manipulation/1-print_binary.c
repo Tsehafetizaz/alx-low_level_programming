498674e68b22d95aef44f1b1d22cfd9e45275878
Tsehafetizaz <wendetefe@gmail.com>
2023-09-29 12:32:13 -0700

    jk

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,32 +7,23 @@
 */
 void print_binary(unsigned long int n)
 {
+unsigned long int mask = 1;
 if (n == 0)
 {
 _putchar('0');
 return;
 }
-
-/* Mask to check each bit */
-unsigned long int mask = 1;
-
-/* Find the position of the MSB */
 while (mask <= n)
 {
 mask <<= 1;
 }
-
-/* Remove the bit left shifted extra from while loop */
 mask >>= 1;
-
-/* Iterate through each bit and print */
 while (mask)
 {
 if (n & mask)
 _putchar('1');
 else
 _putchar('0');
-
 mask >>= 1;
 }
 }
