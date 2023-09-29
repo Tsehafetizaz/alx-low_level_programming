1ede685f0fc23eb4cdb7c6438b6ed43f7894eb9d
Tsehafetizaz <wendetefe@gmail.com>
2023-09-29 13:02:17 -0700

    this

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,17 +7,24 @@
 void print_binary(unsigned long int n)
 {
 unsigned long int mask;
-int should_print = 0; /* Flag to determine when to start printing */
+int should_print = 0;
 
 /* Special case for n = 0 */
 if (n == 0)
 {
 _putchar('0');
-return (0);
+return;
 }
 
 /* Set mask to the most significant bit position */
-mask = 1 << (sizeof(unsigned long int) * 8 - 1);
+if (sizeof(unsigned long int) * 8 <= 32)
+{
+mask = 1UL << 31; /* If the size is 32 bits or less */
+}
+else
+{
+mask = 1UL << 63;
+}
 
 while (mask)
 {
@@ -25,7 +32,8 @@ if (mask & n)
 {
 should_print = 1;
 _putchar('1');
-} else if (should_print)
+}
+else if (should_print)
 {
 _putchar('0');
 }
