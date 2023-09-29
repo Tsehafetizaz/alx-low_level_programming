72ae5ec44c74743f3269401468ba7ca348f295bc
Tsehafetizaz <wendetefe@gmail.com>
2023-09-29 12:17:07 -0700

    test

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,38 @@
+#include "main.h"
+
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
- */
+* print_binary - prints the binary representation of a number.
+* @n: the number
+* Return: nothing
+*/
 void print_binary(unsigned long int n)
 {
-if (n > 1)
+if (n == 0)
 {
-print_binary(n >> 1);
+_putchar('0');
+return;
 }
-_putchar((n & 1) + '0');
+
+/* Mask to check each bit */
+unsigned long int mask = 1;
+
+/* Find the position of the MSB */
+while (mask <= n)
+{
+mask <<= 1;
+}
+
+/* Remove the bit left shifted extra from while loop */
+mask >>= 1;
+
+/* Iterate through each bit and print */
+while (mask)
+{
+if (n & mask)
+_putchar('1');
+else
+_putchar('0');
+
+mask >>= 1;
+}
 }
