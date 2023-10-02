d47ff6eeaa0c013c674e5da2eb3201721f7e2d3c
Tsehafetizaz <wendetefe@gmail.com>
2023-10-02 05:38:38 -0700

    halal

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,53 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "main.h"
 #include <fcntl.h>
-#include <elf.h>
+#include <unistd.h>
 
-/*
-* Displays the information contained in ELF header of the provided ELF file.
-* Parameters:
-* - elf_filename: Name of the ELF file.
-* Return:
-* - Displays the ELF header information and exits with status 0 if successful.
-* - Exits with status code 98 and prints an error message if unsuccessful.
-*/
-int main(int argc, char *argv[])
+/**
+ * Check if the specified file is an ELF file.
+ * This function determines if the file has the ELF
+ * (Executable and Linkable Format) signature.
+ * @param filename Name of the file to check.
+ * @return 1 if ELF, otherwise 0.
+ */
+int is_elf(const char *filename)
 {
 int fd;
-ssize_t read_size;
-Elf64_Ehdr header;
+char buffer[4];
 
-if (argc != 2)
-{
-dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
-exit(98);
-}
-
-fd = open(argv[1], O_RDONLY);
+fd = open(filename, O_RDONLY);
 if (fd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-exit(98);
-}
+return (0);
 
-read_size = read(fd, &header, sizeof(header));
-if (read_size != sizeof(header)
-|| header.e_ident[EI_MAG0] != ELFMAG0
-|| header.e_ident[EI_MAG1] != ELFMAG1
-|| header.e_ident[EI_MAG2] != ELFMAG2
-|| header.e_ident[EI_MAG3] != ELFMAG3)
+if (read(fd, buffer, 4) != 4)
 {
-dprintf(STDERR_FILENO, "%s is not an ELF file\n", argv[1]);
 close(fd);
-exit(98);
+return (0);
 }
 
-printf("ELF Header:\n");
-printf("  Magic:   %.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x "
-"%.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x\n",
-header.e_ident[EI_MAG0], header.e_ident[EI_MAG1],
-header.e_ident[EI_MAG2], header.e_ident[EI_MAG3],
-header.e_ident[EI_CLASS], header.e_ident[EI_DATA],
-header.e_ident[EI_VERSION], header.e_ident[EI_OS...)
+close(fd);
+
+if (buffer[0] == 0x7f && buffer[1] == 'E' && buffer[2] == 'L' && buffer[3] == 'F')
+return (1);
+
+return (0);
 }
