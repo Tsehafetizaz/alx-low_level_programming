e7be36c3d269e2d35699412e66e09d204327403b
Tsehafetizaz <wendetefe@gmail.com>
2023-10-26 04:12:43 -0700

    bilbila

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * print_binary - prints the binary representation of a number
- *   * @n: the number to be printed in binary
- *    */
+ * print_binary - prints the binary representation of a number
+ * @n: the number to be printed in binary
+ */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
