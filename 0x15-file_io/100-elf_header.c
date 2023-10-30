1b710b450a47adebbb4bfcabf06b1ad37efa4c47
Tsehafetizaz <wendetefe@gmail.com>
2023-10-30 07:13:28 -0700

    kkl

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -9,6 +9,7 @@
 void print_magic(unsigned char *e_ident)
 {
 	size_t i;
+
 	printf("  Magic:   ");
 	for (i = 0; i < EI_NIDENT; i++)
 	{
@@ -19,12 +20,16 @@ void print_magic(unsigned char *e_ident)
 
 void print_info(Elf64_Ehdr *header, unsigned char *e_ident)
 {
-	printf("  Class:                             ELF%d\n", e_ident[EI_CLASS] == ELFCLASS32 ? 32 : 64);
-	printf("  Data:                              2's complement, %s\n", e_ident[EI_DATA] == ELFDATA2LSB ? "little endian" : "big endian");
-	printf("  Version:                           %d (current)\n", e_ident[EI_VERSION]);
+	printf("  Class:                             ELF%d\n",
+		e_ident[EI_CLASS] == ELFCLASS32 ? 32 : 64);
+	printf("  Data:                              2's complement, %s\n",
+	e_ident[EI_DATA] == ELFDATA2LSB ? "little endian" : "big endian");
+	printf("  Version:                           %d (current)\n",
+	e_ident[EI_VERSION]);
 	printf("  OS/ABI:                            UNIX - System V\n");
 	printf("  ABI Version:                       %d\n", e_ident[EI_OSABI]);
-	printf("  Type:                              %s\n", header->e_type == ET_EXEC ? "EXEC (Executable file)" : "UNKNOWN");
+	printf("  Type:                              %s\n",
+	header->e_type == ET_EXEC ? \ "EXEC (Executable file)" : "UNKNOWN");
 	printf("  Entry point address:               0x%lx\n", header->e_entry);
 }
 
@@ -53,7 +58,9 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 
-	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 || header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
+	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] !=
+ELFMAG1 || header.e_ident[EI_MAG2] != ELFMAG2 ||
+header.e_ident[EI_MAG3] != ELFMAG3)
 	{
 		fprintf(stderr, "Error: Not an ELF file\n");
 		close(fd);
