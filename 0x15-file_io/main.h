b54b3744de335b0b2eb352c6d8b1d129e2b8eab9
Tsehafetizaz <wendetefe@gmail.com>
2023-10-30 07:25:16 -0700

    tes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,15 +11,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_os_abi(unsigned char *e_ident);
-void print_type(uint16_t e_type, unsigned char *e_ident);
-void print_entry(uint64_t e_entry, unsigned char *e_ident);
-int main(int argc, char **argv);
-
-
 
 #endif /* MAIN_H */
